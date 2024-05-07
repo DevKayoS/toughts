419d2ba155e36094f6e1db6b02f13e31fe7af558
Kayo Vinicius <kayoribeirodev@gmail.com>
2024-05-07 13:12:56 -0300

    build: finalizando a estrutura do projeto

---
+++ README.md
@@ -0,0 +1,17 @@
+# Toughts
+Projeto do curso de node.js no qual vou aprender muitas coisas importantes e uma delas é sobre autentificação que eu realmente gostaria muito de aprender e acho importantissimo
+
+## Pacotes usados:
+  * bcryptjs (gerar senha criptografada e descriptografar ela);
+  * connect-flash (para a conexão do usuário);
+  * cookie-parser (para o usuário se manter logado);
+  * cookie-session (para manter o usuário logado);
+  * express (framework de node);
+  * express-flash (p express entender para fazer uma conexão mais rapida);
+  * express-session (para o express entender que o login por sessão);
+  * sequelize (ORM de banco de dados)
+  * mysql2 (para usar com o sequelize e fazer a conexão com o banco de dados);
+  * nodemon (uma dependencia para fazer as atualizações sem precisar reiniciar o servidor);
+  * session-file-store (para ficar no storage o login do usuário);
+  
+
---
+++ index.js
@@ -0,0 +1,61 @@
+const express = require('express')
+const expbhs = require('express-handlebars')
+// salvar o login na sessão
+const session = require('express-session')
+// salvar a sessão na pasta de sessão
+const FileStore = require('session-file-store')(session)
+// responsavel pelas flash messages
+const flash = require('express-flash')
+// iniciando o express
+const app = express()
+
+// conectando ao banco de dados
+const conn = require('./db/conn')
+// configurando o handlebars
+app.engine('handlebars', expbhs.engine())
+app.set('views engine', 'handlebars')
+
+// receber resposta do body
+app.use(express.urlencoded({
+  extended:true
+}))
+app.use(express.json())
+
+// session middleware
+app.use(
+  session({
+    name: 'session',
+    secret: 'nosso_secret',
+    resave: false,
+    saveUninitialized: false,
+    // configurando o arquivo que vai ficar salvo as sessões do usuário
+    store: new FileStore({
+      logFn: function(){},
+      path: require('path').join(require('os').tmpdir(), 'sessions')
+    }),
+    // configurando o cookie
+    cookie: {
+      secure: false,
+      maxAge: 360000,
+      expires: new Date(Date.now()+360000),
+      httpOnly: true
+    }
+  })
+)
+// flash messages
+app.use(flash())
+
+// public path
+app.use(express.static('public'))
+
+// salvar a session no res
+app.use((req,res, next)=>{
+  if(req.session.userid){
+    res.locals.session = req.session
+  }
+  next()
+})
+
+conn.sync().then(()=>{
+  app.listen(3333)
+}).catch((err)=>console.log(err))
