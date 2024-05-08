9c6af4c827e2f8564725be3afcf86ca8b27b2ba0
Kayo Vinicius <kayoribeirodev@gmail.com>
2024-05-08 14:26:32 -0300

    feat: fazendo o registro do usuário primeira parte

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,3 +1,5 @@
+const User = require('../models/User')
+
 module.exports = class AuthController {
   static login(req,res){
     res.render('auth/login')
@@ -5,5 +7,18 @@ module.exports = class AuthController {
 
   static register(req,res){
     res.render('auth/register')
+  }
+
+  static async registerUser(req,res){
+    const registerUser = {
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+      confirmpassword: req.body.confirmpassword
+    }
+
+    await User.create(registerUser)
+
+    res.redirect('/')
   }
 }
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,5 +5,6 @@ const AuthController = require('../controllers/AuthController')
 
 router.get('/login', AuthController.login)
 router.get('/register', AuthController.register)
+router.post('/register', AuthController.registerUser)
 
 module.exports = router
