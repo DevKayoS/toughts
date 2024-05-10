4b37ee6932c5f947bf268a4b46b51c02fba4f9fb
Kayo Vinicius <kayoribeirodev@gmail.com>
2024-05-10 12:14:01 -0300

    feat: finalizando a autentificação do usuário

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -50,11 +50,34 @@ module.exports = class AuthController {
       console.log(error)
     }
   }
-
+  // deslogando usuário
     static async logoutUser(req,res){
       req.session.destroy()
       res.redirect('/login')
     }
 
+    // fazendo o login do usuário
+    static async loginUser(req,res){
+      const {email, password} = req.body
+      // validando se o usuário existe
+      const user = await User.findOne({where: {email: email}})
+      if(!user){
+        req.flash('message', 'O usuário não foi encontrado')
+        res.redirect('/login')
+        return
+      }
+
+      // validando se a senha enviada pelo formulário é a mesma ques tá no banco de dados
+      const passwordMatch = bcrypt.compareSync(password, user.password)
+      if(!passwordMatch){
+        req.flash('message', 'A senha é inválida')
+        res.redirect('/login')
+        return
+      }
+      req.session.userid = user.id
+      req.session.save(()=>{
+        res.redirect('/')
+      })
+    }
 
 }
