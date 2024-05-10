c6287f746b2c7bab6a28e4927fc8a541d6f7bdf5
Kayo Vinicius <kayoribeirodev@gmail.com>
2024-05-10 13:52:04 -0300

    feat: adicionando um middleware para que seja feita a verificação se o usuário esta logado

--- controllers/ToughtsController.js
+++ controllers/ToughtsController.js
@@ -2,7 +2,12 @@ const Tought = require('../models/Tought')
 const User = require('../models/User')
 
 module.exports = class ToughtsController {
+  // exibindo a home
   static async showToughts(req,res){
     res.render('toughts/home')
+  }
+  // exibindo o dashboard
+  static async dashboard(req,res){
+    res.render('toughts/dashboard')
   }
 }
